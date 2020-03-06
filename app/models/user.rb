c5093c41cb57789c36f9fb567a55d74a02508c02
hiroyoshi-ueda <torikara.h1139@gmail.com>
2020-03-06 07:42:36 +0000

    モデル関連付終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :books
   attachment :profile_image, destroy: false
 
+  has_many :book_comments, dependent: :destroy
+
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, length: {maximum: 20, minimum: 2}
   validates :introduction, length:{ maximum: 50}
